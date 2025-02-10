22727f06bb0354ada1df56bab8dc1f5a68b69901
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-02-10 23:56:05 +0530

    Update 03-Array.js

--- Week 1/1.2 Basic JS APIs (Beginners)/assignments/level-1/03-Array.js
+++ Week 1/1.2 Basic JS APIs (Beginners)/assignments/level-1/03-Array.js
@@ -10,3 +10,39 @@ function pushExample(arr, element) {
     console.log("After push:", arr);
 }
 pushExample([1,2,3],4);
+
+// pop()
+function popExample(arr) {
+    console.log("Original Array:", arr);
+
+    arr.pop();
+    console.log("After shift:", arr);
+}
+popExample([1,2,3]);
+
+// shift()
+function shiftExample(arr) {
+    console.log("Original Array:", arr);
+
+    arr.shift()
+    console.log("After shift:", arr);
+}
+shiftExample([1,2,3]);
+
+// unshit()
+function unshiftExample(arr, element) {
+    console.log("original Array:", arr);
+
+    arr.unshift(element);
+    console.log("After unshift:", arr);
+}
+unshiftExample([1,2,3],0);
+
+// concat()
+function concatExample(arr1, arr2) {
+    console.log("Original Arrays:", arr1, arr2);
+
+    let arr3 = arr1.concat(arr2);
+    console.log("After concat:", arr3);
+}
+concatExample([1,2,3],[4,5,6]);
