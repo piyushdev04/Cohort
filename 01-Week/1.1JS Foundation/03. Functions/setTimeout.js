c0a171b782a26cc48a2aba01bdda0a5f04083fbf
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-03-03 16:12:42 +0530

    Create setTimeout.js

---
+++ 01-Week/1.1JS Foundation/03. Functions/setTimeout.js
@@ -0,0 +1,11 @@
+// setTimeout function example
+
+function greet() {
+    console.log("Hello world");
+}
+
+function greetAlien() {
+    console.log("hello alien");
+}
+
+setTimeout(greetAlien, 3*1000)
