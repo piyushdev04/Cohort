a4e5c9f5d3c057bd40129a00fa493d04b7aa876c
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-03-04 20:59:48 +0530

    Create index.js

---
+++ 03-Week/3.1 Middlewares and global catches/Zod/index.js
@@ -0,0 +1,18 @@
+const zod = require("zod");
+
+// if this is an array of number with atleast 1 input, return true, else return false
+
+function validateInput(obj) {
+    const schema = zod.object({
+        email: zod.string().email(),
+        password: zod.string().min(8)
+    })
+
+    const response = schema.safeParse(obj);
+    console.log(response);
+}
+
+validateInput({
+    email: "piyush@gmail.com",
+    password: "12345678"
+});
